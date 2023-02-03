a0a7c36c029da256d5394198c014b8544ea90dfe
Matt <matta628@gmail.com>
2023-02-03 11:58:43 -0500

    Initialize restaurant project w webpack dev server

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+console.log('lol!');
+
+const greeting = document.createElement('div');
+greeting.textContent = 'Hello World!';
+document.body.appendChild(greeting);
